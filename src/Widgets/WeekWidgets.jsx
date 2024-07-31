e56555502ac6012b3e5cd15bbfe7a437637452e6
Christian Monsalve <chrismon850@gmail.com>
2024-07-31 18:23:38 -0500

    update widget

--- src/Widgets/WeekWidgets.jsx
+++ src/Widgets/WeekWidgets.jsx
@@ -1,14 +1,18 @@
 import styles from './Widgets.module.css'
 
 function WeekWidgets() {
+    const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
     return (
         <>
             <button className={styles.week_btn}>Week</button><br />
             <div className={styles.forecast}>
                 <div className={styles.individual_widget}>
-                    <span>Mon</span>
+                    <div>
+                        <span>Mon</span>
+                    </div>
                     <img src="./src/assets/sunny.png" alt="" />
-                    <span className={styles.temp}></span>
+                    <span className={styles.temp}>23°</span>
                 </div>
             </div>
         </>
