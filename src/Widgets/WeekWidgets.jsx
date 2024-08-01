59e3dfe19b650371be65737938583cfddeec1889
Christian Monsalve <chrismon850@gmail.com>
2024-07-31 23:33:45 -0500

    selected widget template

--- src/Widgets/WeekWidgets.jsx
+++ src/Widgets/WeekWidgets.jsx
@@ -3,16 +3,34 @@ import styles from './Widgets.module.css'
 function WeekWidgets() {
     const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
+
     return (
         <>
             <button className={styles.week_btn}>Week</button><br />
-            <div className={styles.forecast}>
-                <div className={styles.individual_widget}>
-                    <div>
-                        <span>Mon</span>
+            {daysOfWeek.map(day => (
+                <div className={styles.forecast} key={day}>
+                    <div className={styles.individual_widget}>
+                        <div>
+                            <span>{day}</span>
+                        </div>
+                        <img src="./src/assets/sunny.png" alt="sunny" />
+                        <span className={styles.temp}>23°</span>
                     </div>
-                    <img src="./src/assets/sunny.png" alt="" />
-                    <span className={styles.temp}>23°</span>
+                </div>
+            ))}
+            <div className={styles.selected}>
+                <div className={styles.top_div}>
+                    <span>Monday</span> 
+                    <span>10:26 PM</span>
+                </div>
+                <div className={styles.bottom_div}>
+                    <span className={styles.temp}>16°</span>
+                    <img src="./src/assets/sunny.png" alt="sunny" />
+                    <label>Feels Like: 16°</label>
+                </div>
+                <div className={styles.min_max}>
+                    <span>Min: 12°</span>
+                    <span>Max: 12°</span>
                 </div>
             </div>
         </>
